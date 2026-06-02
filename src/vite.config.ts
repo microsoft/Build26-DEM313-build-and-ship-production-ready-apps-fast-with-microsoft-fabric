946f79ecc3b4d8ca2fa941730de925779fd166ab
Christopher Anderson <2252976+christopheranderson@users.noreply.github.com>
2026-06-02 11:17:13 -0700

    Merge pull request #11 from microsoft/christopheranderson/field-technician-template-update
    Update field technician demo to use official template

--- src/vite.config.ts
+++ src/vite.config.ts
@@ -1,16 +1,24 @@
 import tailwindcss from '@tailwindcss/vite';
 import react from '@vitejs/plugin-react-swc';
-import { defineConfig } from 'vite';
 import { resolve } from 'path';
+import { defineConfig } from 'vite';
 
-const projectRoot = process.env.PROJECT_ROOT || import.meta.dirname;
-
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
-      '@': resolve(projectRoot, 'src'),
+      '@': resolve(import.meta.dirname, 'src'),
+    },
+  },
+  build: {
+    target: 'es2022',
+  },
+  esbuild: {
+    target: 'es2022',
+  },
+  optimizeDeps: {
+    esbuildOptions: {
+      target: 'es2022',
     },
   },
 });
